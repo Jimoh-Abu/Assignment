17add755dd3bfc74b9d1ad6c6db40ea8ae22cf5d
Jimoh-Abu <45285278+Jimoh-Abu@users.noreply.github.com>
2018-11-24 23:55:13 +0100

    Add files via upload

---
+++ Grade_calc.py
@@ -0,0 +1,22 @@
+print("GRADE CALCULATOR")
+x = int(input("score"))
+if (x>=70 and x<=100):
+    print("A")
+elif(x>=60 and x<=69):
+    print("B")
+elif(x>=50 and x<=59):
+    print("C")
+elif(x>=45 and x<=49):
+    print("D")
+elif(x>=40 and x<=44):
+    print("E")
+elif(x>=0 and x<=39):
+    print("F")
+else:
+    print("does not have a grade")
+
+
+
+
+
+
