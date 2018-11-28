23f4cb12c739edad73c61f3c54f2b3adb0e79c2e
Jimoh-Abu <45285278+Jimoh-Abu@users.noreply.github.com>
2018-11-28 12:39:33 +0100

    Add files via upload

---
+++ rad.py
@@ -0,0 +1,18 @@
+
+x = 1
+while(x<=100):
+    if(x%2==0):
+        print("fizz")
+    elif(x%3==0):
+        print("buzz")
+        if(x%3==0 and x%5==0):
+            print("fizz buzz")
+        
+
+    else:
+        print(x)
+    x+=1
+      
+    
+
+ 
